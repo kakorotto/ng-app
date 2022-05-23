d76f6c15823cb7fb6f7597d690dab8a2e295fbbd
EC2 Default User <ec2-user@ip-172-31-14-116.ec2.internal>
2022-05-23 12:56:45 +0000

    adding components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderContainerComponent } from './header-container/header-container.component';
+import { HeaderComponent } from './header/header.component';
+import { NavComponent } from './nav/nav.component';
+import { HomeComponent } from './home/home.component';
+import { AsideComponent } from './aside/aside.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderContainerComponent,
+    HeaderComponent,
+    NavComponent,
+    HomeComponent,
+    AsideComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
