55f8fbd81b74ee48182c3f83703a8b3d743e5d2f
kakorotto <paulaatiya@gmail.com>
2022-05-24 07:57:47 +0200

    added ngFor for countries and people name

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,13 +3,31 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
+  peopleByCountry: any[] = [
+    {
+      country: 'UK',
+      people: [{ name: 'Douglas Pace' }, { name: 'Mcleod Mueller' }],
+    },
 
-  constructor() { }
+    {
+      country: 'US',
+      people: [
+        { name: 'Day Meyers' },
+        { name: 'Aguirre Ellis' },
+        { name: 'Cook Tyson' },
+      ],
+    },
 
-  ngOnInit(): void {
-  }
+    {
+      country: 'MARS',
+      people: [{ name: 'Douglas Pace' }, { name: 'Mcleod Mueller' }],
+    },
+  ];
 
+  constructor() {}
+
+  ngOnInit(): void {}
 }
